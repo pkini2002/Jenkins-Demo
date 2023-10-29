08b242372553cf80106baa60f0b58898565298b7
pkini2002 <gpkini2002@gmail.com>
2023-10-29 15:26:22 +0530

    error made

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello{
          System.out.println("I am testing my code using Jenkins");
          System.out.println("I am testing my code using Jenkins again");
 
-         System.out.println("One more line of code")
+         Sys.ou.println("One more line of code")
     }
 }
