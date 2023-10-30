420e3ac037424fb592030d5da9061f78b1c5e051
Prathiksha Kini <84091455+pkini2002@users.noreply.github.com>
2023-10-30 23:24:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello{
          System.out.println("Hello World");
          System.out.println("I am testing my code using Jenkins");
          System.out.println("I am testing my code using Jenkins again");
-         Sys.ou.println("One more line of code")
+         System.out.println("One more line of code");
     }
 }
